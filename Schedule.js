7a29b07c6288f2dfb8bc4c109d9e25723a1da1c6
jesse keane <me@jessekeane.me>
2013-10-24 16:36:31 -0700

    Schedule to style

--- Schedule.js
+++ Schedule.js
@@ -1,38 +1,43 @@
-exports.Schedule = function Schedule(){
+exports.Schedule = Schedule
 
-    this.name    
-    this.date
-    this.push
+function Schedule() {
+  this.name = null
+  this.date = null
+  this.push = null
+}
 
-    this.setName = function(name){
-        this.name = name
-    }
-    
-    this.setDate = function(date){
-        this.date = date
-    }
-    
-    this.setPush = function(push){
-        this.push = push
-    }
-    
-    this.toJSON = function(){
-        
-        var payload = {}
-        
-        if (this.name !== undefined) {
-            payload.name = this.name
-        }
-        
-        payload.schedule = {}
-        
-        // to JSON() gives milliseconds, truncate them off, this is ugly, but effective and keeps in line with the other usages of the date
-        payload.schedule.scheduled_time = this.date.toJSON().substring(0,this.date.toJSON().length-5)   
-        
-        payload.push = this.push.toJSON()
-        
-        return payload
-        
-    }
-    
+var cons = Schedule
+  , proto = cons.prototype
+
+proto.constructor = cons
+
+proto.setName = function(name) {
+  this.name = name
+}
+
+proto.setDate = function(date) {
+  this.date = date
+}
+
+proto.setPush = function(push) {
+  this.push = push
+}
+
+proto.toJSON = function() {
+  var payload = {}
+
+  if(this.name !== undefined) {
+    payload.name = this.name
+  }
+
+  payload.schedule = {}
+
+  // to JSON() gives milliseconds, truncate them off, this is ugly,
+  // but effective and keeps in line with the other usages of the date
+  payload.schedule.scheduled_time = this.date.toJSON()
+      .substring(0, this.date.toJSON().length - 5)
+
+  payload.push = this.push.toJSON()
+
+  return payload
 }
