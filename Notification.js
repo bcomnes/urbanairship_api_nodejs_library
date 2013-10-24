dafd319157bcf5076332c9dccff79fdd92930ae6
jesse keane <me@jessekeane.me>
2013-10-24 16:45:34 -0700

    Notification to style

--- Notification.js
+++ Notification.js
@@ -1,68 +1,70 @@
-exports.Notification = function Notification() {
-    
-    this.deviceType
-    this.alert
-        
-    // ios specific
-    this.badge
-    this.sound
-    this.content_available
-    this.expiry
-    this.priority
-    
-    // android specific
-    this.collapse_key
-    this.time_to_live
-    this.delay_while_idle
-    
-    // ios and Android 
-    this.extras = []
-    
-    this.setDeviceType = function(deviceType){
-        this.deviceType = deviceType
-    }
+exports.Notification = Notification
 
-    this.setAlert = function(alert){
-        this.alert = alert
-    }
-    
-    // ios specific settings
-    this.setBadge = function(badge){
-        this.badge = badge      
-    }
+function Notification() {
+  this.deviceType = null
+  this.alert = null
+  // ios specific
+  this.badge = null
+  this.sound = null
+  this.content_available = null
+  this.expiry = null
+  this.priority = null
+  // android specific
+  this.collapse_key = null
+  this.time_to_live = null
+  this.delay_while_idle = null
+  // ios and Android
+  this.extras = []
+}
 
-    this.setSound = function(sound){
-        this.sound = sound
-    }
-    
-    this.setContentAvailable = function(content_available){
-        this.content_available = content_available
-    }
-    
-    this.setExpiry = function(expiry){
-        this.expiry = expiry
-    }
-    
-    this.setPriority = function(priority){
-        this.priority = priority
-    }
-    
-    // android specific settings
-    this.setCollapseKey = function(collapse_key){
-        this.collapse_key = collapse_key
-    }
-    
-    this.setTimeToLive = function(time_to_live){
-        this.time_to_live = time_to_live
-    }
-    
-    this.setDelayWhileIdle = function(delay_while_idle){
-        this.delay_while_idle = delay_while_idle
-    }
-    
-    // both ios and android specific
-    this.addExtra = function(k,v){
-        this.extras.push({ key: k, value: v})
-    }
-    
+var cons = Notification
+  , proto = cons.prototype
+
+proto.constructor = cons
+
+proto.setDeviceType = function(device_type) {
+  this.deviceType = device_type
+}
+
+proto.setAlert = function(alert) {
+  this.alert = alert
+}
+
+// ios specific settings
+proto.setBadge = function(badge) {
+  this.badge = badge
+}
+
+proto.setSound = function(sound) {
+  this.sound = sound
+}
+
+proto.setContentAvailable = function(content_available) {
+  this.content_available = content_available
+}
+
+proto.setExpiry = function(expiry) {
+  this.expiry = expiry
+}
+
+proto.setPriority = function(priority) {
+  this.priority = priority
+}
+
+// android specific settings
+proto.setCollapseKey = function(collapse_key) {
+  this.collapse_key = collapse_key
+}
+
+proto.setTimeToLive = function(time_to_live) {
+  this.time_to_live = time_to_live
+}
+
+proto.setDelayWhileIdle = function(delay_while_idle) {
+  this.delay_while_idle = delay_while_idle
+}
+
+// both ios and android specific
+proto.addExtra = function(k, v) {
+  this.extras.push({ key: k, value: v})
 }
